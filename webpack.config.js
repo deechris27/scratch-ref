1baf839d51c57fdf4403d8aa59973a4e4952d250
Deepak <dd@dd.com>
2020-02-02 12:37:20 +0530

    scaffolding done - hello deepak

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: './src/index.js',
+
+   output:{
+       path: path.join(__dirname, '/dist'),
+       filename: 'index_bundle.js'
+   },
+
+  devtool: "source-map",
+
+  module:{
+      rules:[
+          {test: /\.js$/, use: 'babel-loader', exclude: [path.resolve(__dirname, "node_modules")]},
+          {test: /\.jsx$/, use: 'babel-loader', exclude: [path.resolve(__dirname, "node_modules")]},
+          {test: /\.scss$/, use: 'sass-loader', exclude: [path.resolve(__dirname, "node_modules")]}
+      ]
+  },
+
+  plugins:[
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+};
